1d78b4bbaa88f62a6e89daa66b2c4ccca65ee22f
SONALIK12 <100865085+SONALIK12@users.noreply.github.com>
2023-07-06 17:24:48 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Link,
+  Outlet,
+  createRoutesFromElements,
+} from "react-router-dom";
+import Products from "./pages/Products";
+import Home from "./pages/Home";
+import Reports from "./pages/Reports";
+import Navbar from "./components/sidebar";
+import "./App.css";
+
+const AppLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+  </>
+);
+
+
+
+const router = createBrowserRouter([
+  {
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "products",
+        element: <Products />,
+      },
+      {
+        path: "reports",
+        element: <Reports />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
+);
