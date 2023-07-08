8325db7a316b8f876578dc5d664ef67241850e12
SONALIK12 <100865085+SONALIK12@users.noreply.github.com>
2023-07-08 21:40:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,48 +1,17 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-  Route,
-  Link,
-  Outlet,
-  createRoutesFromElements,
-} from "react-router-dom";
-import Products from "./pages/products";
-import Home from "./pages/home";
-import Reports from "./pages/reports";
-import Navbar from "./components/sidebar";
-import "./App.css";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-const AppLayout = () => (
-  <>
-    <Navbar />
-    <Outlet />
-  </>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
-
-
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "products",
-        element: <Products />,
-      },
-      {
-        path: "reports",
-        element: <Reports />,
-      },
-    ],
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
-  <RouterProvider router={router} />
-);
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
